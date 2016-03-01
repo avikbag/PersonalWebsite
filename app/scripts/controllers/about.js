8eac1e6ff217123248f0576a0bea7290f97695c7
Avik Bag <avikbag@Aviks-MacBook-Pro.local>
2016-03-01 00:41:04 -0500

    Initial Upload

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name resumeAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the resumeAppApp
+ */
+angular.module('resumeAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name resumeAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the resumeAppApp
+ */
+angular.module('resumeAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
