1a2883b28f2b93d51c8edbba3289b61f80ba3212
Avik Bag <avikbag@Aviks-MacBook-Pro-2.local>
2016-08-29 14:17:13 -0400

    test8

--- web.js
+++ web.js
@@ -1,13 +1,8 @@
-var express = require('express');
-var http = require('http');
 var gzippo = require('gzippo');
-
+var express = require('express');
+var morgan = require('morgan');
 var app = express();
-app.use(gzippo.staticGzip('' + __dirname));
-console.log(__dirname);
-app.use('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
 
-var server = http.createServer(app);
-server.listen(process.env.PORT || 9000);
+app.use(morgan('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
