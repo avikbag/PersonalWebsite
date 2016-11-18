a9ae7aa85ada9c0a53f39ce7cb9caa75deecb2f0
Avik Bag <avikbag@gmail.com>
2016-11-17 21:45:16 -0500

    Added transitions and content layout for about me

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,6 +20,38 @@ angular
     'ngTouch', 
     'ngMaterial'
   ])
+  .config(function ($mdThemingProvider){
+    // If you specify less than all of the keys, it will inherit from the
+    // default shades
+    $mdThemingProvider.definePalette('custom', {
+    '50': 'rgba(84, 110, 122, 0.75)',
+    '100': 'ffcdd2',
+    '200': 'ef9a9a',
+    '300': 'e57373',
+    '400': 'ef5350',
+    '500': 'f44336',
+    '600': 'e53935',
+    '700': 'd32f2f',
+    '800': 'c62828',
+    '900': 'b71c1c',
+    'A100': 'ff8a80',
+    'A200': 'ff5252',
+    'A400': 'ff1744',
+    'A700': 'd50000',
+    'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+                                        // on this palette should be dark or light
+    'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+     '200', '300', '400', 'A100'],
+    'contrastLightColors': undefined    // could also specify this if default was 'dark'
+  });
+    $mdThemingProvider.theme('default')
+    .primaryPalette('orange', {
+      'default': '800'
+    })
+    .backgroundPalette('custom', {
+      'default': '50' // use shade 200 for default, and keep all other shades the same
+    });
+  })
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
@@ -34,5 +66,5 @@ angular
       })
       .otherwise({
         redirectTo: '/'
-      })
+      });
   });
